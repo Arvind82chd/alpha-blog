40a20f0e454a3ac7f7e2487c71f0d9c641b2365d
Arvind Sharma <arvindsharma@Arvinds-MacBook-Air.local>
2023-04-24 15:48:37 -0700

    add links to interconnect pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
   def edit
     @article = Article.find(params[:id])
   end
+  
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
@@ -34,6 +35,12 @@ class ArticlesController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
 
