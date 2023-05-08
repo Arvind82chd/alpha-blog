6fcbce7674b006eb4892db7833708f84e79e60a5
Arvind Sharma <arvindsharma@Arvinds-MacBook-Air.local>
2023-05-08 12:54:25 -0600

    add controller methods as helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user, :logged_in? #to make it available to views also not just controllers.
+    
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id] 
+    end
+
+    def logged_in?
+      !!current_user
+    end
+      
 end
