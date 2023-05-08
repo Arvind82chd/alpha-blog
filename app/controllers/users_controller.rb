1f8b0a3cb9eec3f461512bac426612605d6a67ba
Arvind Sharma <arvindsharma@Arvinds-MacBook-Air.local>
2023-05-08 12:39:07 -0600

    add authentication helpers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
-    def new
+  before_action :set_user, only: [:show, :edit, :update]  
+  
+  def new
         @user = User.new
     end
 
     def create
       @user = User.new(user_params)
       if @user.save
+        session[:user_id] = @user.id #to keep the new user signed in.
         flash[:notice] = "Welecome #{@user.username}, You successfully signed up for Alpha Blog"
         redirect_to articles_path
       else
@@ -14,11 +17,10 @@ class UsersController < ApplicationController
     end
 
     def edit
-      @user = User.find(params[:id])
     end
 
     def update
-      @user = User.find(params[:id])
+      
       if @user.update(user_params)
         flash[:notice] = "Your account info was updated successfully"
         redirect_to @user
@@ -28,19 +30,21 @@ class UsersController < ApplicationController
     end
 
     def show
-      @user = User.find(params[:id])
       @articles = @user.articles.paginate(:page => params[:page], :per_page => 5)
     end
 
 
     def index
       @users = User.paginate(:page => params[:page], :per_page => 5)
-      # @users = User.all
     end
 
     private
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
     end
 
 end
