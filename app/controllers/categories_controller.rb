9b513cdcfec56ef021fb367a2637c8a7e70b9129
Arvind Sharma <arvindsharma@Arvinds-MacBook-Air.local>
2023-05-10 20:22:01 -0600

    add association from UI

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -26,7 +26,7 @@ class CategoriesController < ApplicationController
     private
 
     def category_params
-      params.require(:category).permit(:name)
+      params.require(:category).permit(:name, article_ids: [])
     end
 
     def require_admin
